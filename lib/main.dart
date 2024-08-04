db71681fc8963443b390775f3c9262ce546bfc73
Tanish-Inamdar <tanishdinamdar@gmail.com>
2024-08-04 14:26:10 -0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'screens/home_screen.dart';
-// import '../models/ai.dart';
-// // import 'package:firebase_core/firebase_core.dart';  
-
-// Future<void> main() async{
-//   WidgetsFlutterBinding.ensureInitialized();
-//   runApp(
-//     const Home(),
-//   );
-// }
-
-// class Home extends StatelessWidget{
-//   const Home({Key? key}) :super(key:key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const MaterialApp(
-//       debugShowCheckedModeBanner: false, 
-//       home: HomeScreen(),
-//       //home: Home() //MAKE SURE TO CHANGE BACK
-//     );
-//   } 
-// }
 
 
 import 'package:flutter/material.dart';
